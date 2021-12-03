e8e4ab3944d8b4243aaf30b113e54582c2e76397
sreyache <91205378+sreyache@users.noreply.github.com>
2021-12-03 06:28:35 -0500

    changes to json file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 
 let daysofyear = []
 
-fetch("https://sreyache.github.io/pui-final-project/assets/daysofyear.json")
+//READ JSON FILE
+fetch("https://sreyache.github.io/pui-final-project/assets/2022_weekends.json")
   .then(response => response.json())
   .then(function(data) {
       daysofyear = data;
+
 
       console.log(typeof daysofyear[0]);
       console.log(daysofyear[0].dow);
@@ -12,21 +14,39 @@ fetch("https://sreyache.github.io/pui-final-project/assets/daysofyear.json")
   .catch(error => console.log(error));
 
 
-  function changeCalendar() {
-      console.log("calendar changed");
-      
-      var month = document.getElementById("month-selector").value;
-      console.log(month);
+//DYNAMICALLY CHANGE CALENDAR IMAGE BASED ON INPUT FROM DROPDOWN
+function changeCalendar() {
+    console.log("calendar changed");
 
-      var pathname = "img/calendar/" + month + ".png";
-   
-      var image = document.getElementById('calendar_image');
-      image.src = pathname;
-      
-  }
+    var month = document.getElementById("month-selector").value;
+    console.log(month);
 
+    var pathname = "img/calendar/" + month + ".png";
 
-function weekend(friday, saturday, availability) {
-    console.log("the weekend");
+    var image = document.getElementById('calendar_image');
+    image.src = pathname;
+    
+    console.log(daysofyear.len);
 }
+
+//DATE CONSTRUCTOR
+// function Date (day, month, dow) {
+//     console.log("the weekend");
+// }
+
+//MATCHING WEEKENDS 
+//FIND THE FIRST FRIDAY
+
+
+
+//WEEKEND CONSTRUCTOR
+// function Weekend(friday, saturday, availability) {
+//     console.log("the weekend");
+// }
+
+//REPLACE LOCATION FIELD WITH SUBMITTED CONTENT
+
+// function replaceField() {
+
+// }
 
