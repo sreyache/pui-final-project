130aac71bd1ed4d87bec43d78ab06500f3758fbb
sreyache <91205378+sreyache@users.noreply.github.com>
2021-12-03 07:20:31 -0500

    ability to find weekends in a given month

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 
-let daysofyear = []
+let weekends = []
 
 //READ JSON FILE
 fetch("https://sreyache.github.io/pui-final-project/assets/2022_weekends.json")
   .then(response => response.json())
   .then(function(data) {
-      daysofyear = data;
+      weekends = data;
+      console.log(weekends[0].month1);
 
-
-      console.log(typeof daysofyear[0]);
-      console.log(daysofyear[0].dow);
+      console.log(typeof weekends[0]);
+      console.log(weekends[0].dow1);
+      console.log(weekends.length);
   })
   .catch(error => console.log(error));
 
@@ -25,9 +26,30 @@ function changeCalendar() {
 
     var image = document.getElementById('calendar_image');
     image.src = pathname;
-    
-    console.log(daysofyear.len);
 }
+
+function findDates(month) {
+    var given_month = month;
+    var monthweekend = []
+
+    for (var i in weekends) {
+        if (weekends[i].month1 == given_month) {
+            monthweekend.push(weekends[i]);
+            console.log("weekends-found")
+        }
+    }
+   return monthweekend; 
+}
+
+function load() {
+    var current_month = document.getElementById("month-selector").value;
+    var live_weekends = findDates(current_month);
+
+    console.log(live_weekends);
+
+}
+
+
 
 //DATE CONSTRUCTOR
 // function Date (day, month, dow) {
@@ -38,11 +60,6 @@ function changeCalendar() {
 //FIND THE FIRST FRIDAY
 
 
-
-//WEEKEND CONSTRUCTOR
-// function Weekend(friday, saturday, availability) {
-//     console.log("the weekend");
-// }
 
 //REPLACE LOCATION FIELD WITH SUBMITTED CONTENT
 
