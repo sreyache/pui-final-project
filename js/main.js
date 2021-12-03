34195bf28d6a787ada022d3a3e8bc173735aff27
sreyache <91205378+sreyache@users.noreply.github.com>
2021-12-03 04:23:49 -0500

    reorganize files

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 let daysofyear = []
 
-fetch("https://sreyache.github.io/pui-final-project/daysofyear.json")
+fetch("https://sreyache.github.io/pui-final-project/assets/daysofyear.json")
   .then(response => response.json())
   .then(function(data) {
       daysofyear = data;
+
       console.log(typeof daysofyear[0]);
       console.log(daysofyear[0].dow);
   })
   .catch(error => console.log(error));
+
 
