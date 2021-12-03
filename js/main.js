e413a9530f193b7971c6f2caf5a170a96c42d1a4
sreyache <91205378+sreyache@users.noreply.github.com>
2021-12-03 04:00:11 -0500

    trying getch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 
-const data = require('../daysofyear.json');
-console.log(data);
+// const data = require('../daysofyear.json');
+// console.log(data);
+
+
+let response = fetch(https://sreyache.github.io/pui-final-project/daysofyear.json)
+
+fetch("../pui-final-project/daysofyear.json")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
