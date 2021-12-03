b1ab5f0d25d87575690246198dda6141aecd1933
sreyache <91205378+sreyache@users.noreply.github.com>
2021-12-03 04:03:16 -0500

    fetch

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
 // console.log(data);
 
 
-let response = fetch(https://sreyache.github.io/pui-final-project/daysofyear.json)
-
-fetch("../pui-final-project/daysofyear.json")
+fetch("https://sreyache.github.io/pui-final-project/daysofyear.json")
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.log(error));
